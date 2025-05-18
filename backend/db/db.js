10c9b62cc471d6037c41d1f2715b73b33667b8b1
chetanmadiwalar <chetanmadiwalar1947@gmail.com>
2025-05-19 00:10:37 +0530

    Updated

--- backend/db/db.js
+++ backend/db/db.js
@@ -5,7 +5,10 @@ const MONGO_URL = process.env.MONGO_URL
 const db = async () => {
     try {
         mongoose.set('strictQuery', false)
-        await mongoose.connect(MONGO_URL)
+        await mongoose.connect(MONGO_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
         console.log('Db Connected')
     } catch (error) {
         console.log(`${error}`);
